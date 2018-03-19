ffc6ab169f477c02a96d7770da6d2799bc688409
Chelsea G <chelseagieck@Chelseas-MacBook-Pro.local>
2018-03-18 23:01:47 -0400

    fixes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,27 @@
+// Original Game Board Values
 var wins = 0;
 var losses = 0;
 var gameRandom = Math.floor((Math.random() * 200) + 1);
 var lives = 25;
+var userNumberStart = 0;
 var val1 = Math.floor((Math.random() * 50) + 1);
 var val2 = Math.floor((Math.random() * 20) + 1);
 var val3 = Math.floor((Math.random() * 30) + 1);
 var val4 = Math.floor((Math.random() * 10) + 1);
+var drink1 = '';
+var drink2 = '';
+var drink3 = '';
+var drink4 = '';
 
+// Original Functions
+var setWins = function(){$('#win').html(wins)};
+var setLost = function(){$('#lost').html(losses)};
+var setRandom = function(){$('#gameNumber').html(gameRandom)};
+var setLives = function(){$('#guess').html(lives)};
+var setUserNumber = function(){$('#userNumber').html(userNumberStart)};
 
-var setWins = $('#win').html(wins);
-var setLost = $('#lost').html(losses);
-var setRandom = $('#gameNumber').html(gameRandom);
-var setLives = $('#guess').html(lives);
+// Original Game Controls
+/* var drink1 = function(){$('#drink1').on("click", function(){$('#userNumber').html(userNumberStart + val1)})}; */
 
 
 $(document).ready(function(){
@@ -21,12 +31,13 @@ $(document).ready(function(){
     console.log(val2);
     console.log(val3);
     console.log(val4);
-    var setWins = $('#win').html(wins);
-    var setLost = $('#lost').html(losses);
-    var setRandom = $('#gameNumber').html(gameRandom);
-    var setLives = $('#guess').html(lives);
-    setWins();
-    setLost();
-    setRandom();
-    setLives();
+
+
+    $('#start').click(function(){
+        setWins();
+        setLost();
+        setRandom();
+        setLives();
+        setUserNumber();
+    });
 });
